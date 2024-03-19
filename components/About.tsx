197dbee14279572d23dff2dac21aa54c41096dd7
caglenoah <caglenoah@gmail.com>
2024-03-19 17:10:28 -0600

    Complete first personalization of the Hero.tsx(landing page)

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import MainImage from "../images/MainImage.jpeg";
 
 type Props = {};
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import Image from "next/image";
+import MainImage from "../images/MainImage.jpeg";
 import BackgroundCircles from "./BackgroundCircles";
 import Link from "next/link";
 
@@ -8,9 +10,9 @@ type Props = {};
 export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
-      "The names Noah",
-      "Im a real mans man",
-      "I like beer and objectifying women",
+      "I'm Noah!",
+      "I love pushing my comfort zone",
+      "and using different skill sets to help people :)",
     ],
     loop: true,
     delaySpeed: 2000,
@@ -22,10 +24,10 @@ export default function Hero({}: Props) {
     text-center overflow-hidden"
     >
       <BackgroundCircles />
-      <img
+      <Image
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
-        src="https://lh3.googleusercontent.com/pw/AP1GczNTkjHKXt6iF7mIAbUEjypqdyyUwdiL-rlDd_-Tt2Xpl-6tiXphYAKIaYhOXQYFaB38d72xaUMrlHg5wn0_8rV2gx9aiedowZlrWD7KKmGLHJ6D_2zsOl2mHLcRtcAL4ELrGBVNk-bWAmZbfno96BsLe7sUtUSAS7RmMUuj_5yyw3YKSnNoJTTfTZ4dZYz44FTOONAMNZUFZEsmUWDvmzF8pxiOxdOWYYQRvRt5sOcnLt_EwKckOBR0i1jdK-9qARuEQhM6SkNWcEYBqje3gBR2hgX45S3W_RllbkeLsdFVkSLVBA5nkCUSbjqZaccLDZ7K1WW2NASrLOPSEC36RC55nDINF98onhZWkHQRLt980PLKwCikMZE_pgbrH2hbGHtdUYps9OyozezL4jZuXQjGcAIKSeZ7JXPgVcqefyFLaalDKrLC92Fl6NPs0qv7MX3z8aeYd3XXZwBmqp4H3tYLKNrkKrq3cCMrcEY3xiH6XoJYx6RdhXMgt_4yqepO5W2SQu_bZIfve1Vx_vRbLwutcybSSrYIW--flUgN5wHwoBphwSYicI3Ig1dxIs4J-4JmTB9ou4tRUwuRoBryEjjoAb9FoPDinFmvfHUT0PqyaCSl9VKjxitFgKUnCOPc1zJMV9BCAtdY3a-UzsOqGGScEhSsVv5hAoiUOwhGMZjqDAD-ENqTdUcM-eMj4jx8IsesULsEZn9wZ-tGUpBW_JOIv7zgBTh9cPoH6jBi2sTp3YeJoxCU527nzekzEOnc_Bz2eaVNkvSpQMe3M3XWe5X_rFn1zJUaCM4aqV6ZLkgbftsRZSp9RVGyVqQImgLlt3laQv5tiOJYVzSiZXXOZWe-xSThvDbsu8eSS6ey4zIOxXfBAJatKgXnRf_gAdL0xZ8sYs3M-bvKiF2LvDL054s=w1068-h1424-s-no-gm?authuser=0"
-        alt=""
+        src={MainImage}
+        alt="Main Image"
       />
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
