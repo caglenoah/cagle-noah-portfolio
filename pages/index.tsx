39c4fd0db5b0f3c9a8451bd9435d326ec66c7541
caglenoah <caglenoah@gmail.com>
2024-03-19 14:10:37 -0600

    initializeing correct install

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Noah's Portfolio</title>
+      </Head>
+
+      <h1 className=" p-10 text-red-500">Let's build!!</h1>
+    </div>
+  );
+};
+
+export default Home;
