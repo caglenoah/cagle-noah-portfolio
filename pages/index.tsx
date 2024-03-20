10241474c1c04e832f61ff84093b4ccc1fd618a3
caglenoah <caglenoah@gmail.com>
2024-03-20 16:59:43 -0600

    created added photo to the footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
 import Link from "next/link";
+import Image from "next/image";
+import MainImage from "../images/MainImage.jpeg";
 
 const Home: NextPage = () => {
   return (
@@ -47,10 +49,10 @@ const Home: NextPage = () => {
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            <img
+            <Image
+              src={MainImage}
+              alt="footerimage"
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0"
-              src=""
-              alt=""
             />
           </div>
         </footer>
